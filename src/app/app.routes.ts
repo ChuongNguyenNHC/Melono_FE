5da0c151cd38f27feeecd871829940f293131775
JohanCorey <nguyenhoangchuong5541@gmail.com>
2026-04-07 20:14:09 +0700

    feat: implement login/register page, forgot password gage, add boxicons library docs: update README

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'forget',
+    title: 'Quên mật khẩu | Melono',
+    loadComponent: () => import('./auth/forget/forget').then(m => m.Forget)
+  },
+  {
+    path: 'login',
+    title: 'Đăng nhập | Melono',
+    loadComponent: () => import('./auth/login/login').then(m => m.Login)
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  }
+];
