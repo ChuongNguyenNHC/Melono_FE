774b5d02e27ced05464c087459955f69fa9e9c59
Hieu Nguyen <hieunguyendoan8@gmail.com>
2026-04-09 15:18:40 +0700

    bổ sung chức năng sidebar của playlist

--- src/app/playlistsbar/playlistsbar.ts
+++ src/app/playlistsbar/playlistsbar.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+export interface MyPlaylist {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-playlistsbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './playlistsbar.html',
   styleUrl: './playlistsbar.css',
 })
-export class Playlistsbar {}
+export class Playlistsbar {
+  isExpanded = true;
+  
+  playlists: MyPlaylist[] = [
+    { id: 1, name: 'Nhạc Chilling' },
+    { id: 2, name: 'Top Hits Việt Nam' },
+    { id: 3, name: 'US-UK EDM Mix' }
+  ];
+
+  toggleSidebar() {
+    this.isExpanded = !this.isExpanded;
+  }
+
+  createPlaylist() {
+    const newId = this.playlists.length > 0 ? Math.max(...this.playlists.map(p => p.id)) + 1 : 1;
+    this.playlists.push({ id: newId, name: `Playlist số ${newId}` });
+  }
+}
